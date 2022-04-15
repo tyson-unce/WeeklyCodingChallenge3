e91c51227ee4a9b73988b82cb1fa4820d672501e
Tyson Unce <Tyson@MacBook-Pro.attlocal.net>
2022-04-15 09:31:04 -0500

    updated functions

--- Weekly_Coding_Challenge_3/VeryHardChall.js
+++ Weekly_Coding_Challenge_3/VeryHardChall.js
@@ -1,18 +1,44 @@
+function add(p1, p2)
+    {
+        return p1 + p2;
+    }
+
+
+function subtract(p1, p2)
+    {
+        return p1 - p2;
+    }
+
+
+function multiply(p1, p2)
+    {
+        return p1*p2;
+    }
+
+
+function divide(p1, p2)
+    {
+        return p1/p2;
+    }
+
+
+
+
 let inputNum1 = prompt("Enter your first number");
 let inputOp = prompt("Enter an operator out of +, -, *, /: ");
 let inputNum2 = prompt("Enter your second number");
 
 if (inputOp == "+"){
-    alert(Number(inputNum1)+Number(inputNum2))
+    alert(add(Number(inputNum1), Number(inputNum2)))
 }
 else if(inputOp == "-"){
-    alert(Number(inputNum1)-Number(inputNum2))
+    alert(subtract(inputNum1, inputNum2))
 }
 else if(inputOp == "*"){
-    alert(Number(inputNum1)*Number(inputNum2))
+    alert(multiply(inputNum1, inputNum2))
 }
 else if(inputOp == "/"){
-    alert((Number(inputNum1))/(Number(inputNum2)))
+    alert(divide(inputNum1, inputNum2))
 }
 else{
     alert("ERROR: INVALID OPERATOR/NUMBER")
